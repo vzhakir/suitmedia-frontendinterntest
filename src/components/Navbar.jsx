98920b65cfdad4571b1b44816d84deea4022954c
vzhakir <zhakischool@gmail.com>
2025-07-22 15:58:15 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,10 @@ import logo from '../assets/logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-  // State untuk efek scroll
   const [isTransparent, setIsTransparent] = useState(false);
   const [isHidden, setIsHidden] = useState(false);
   const lastScrollTop = React.useRef(0);
-  
-  // State untuk menu hamburger
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // Efek untuk handle scroll
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -37,19 +32,16 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  // Fungsi untuk toggle menu hamburger
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Menggabungkan semua class dinamis
   const navClasses = `${styles.navbar} ${isTransparent ? styles.transparent : ''} ${isHidden ? styles.hidden : ''}`;
-
   return (
     <nav className={navClasses}>
-      <Link to="/" className={styles.logo}>
+      <span className={styles.logo}>
         <img src={logo} alt="Suitmedia Logo" className={styles.logoImg} />
-      </Link>
+      </span>
       
       <div className={`${styles.navLinks} ${isMenuOpen ? styles.navMenuOpen : ''}`}>
         <NavLink to="/work" onClick={toggleMenu} className={({ isActive }) => isActive ? styles.active : ''}>Work</NavLink>
